d2bc3703415c9aefb33ff2ebb3a7fe6ddfec81da
Guilherme G Likoski <159035705+GuilhermeGLikoski@users.noreply.github.com>
2026-05-22 14:53:36 -0300

    Create exercicio6.py
    6. Escreva um programa que crie duas listas, uma com os números pares de 1 a 10
    e outra com os números ímpares de 1 a 10. Em seguida, junte as duas listas em
    uma terceira lista e a imprima na tela.

---
+++ exercicio6.py
@@ -0,0 +1,7 @@
+pares = [2, 4, 6, 8, 10]
+impares = [1, 3, 5, 7, 9]
+terceira_lista = pares + impares
+
+print("Lista de Pares:", pares)
+print("Lista de Ímpares:", impares)
+print("Terceira lista juntada:", terceira_lista)
