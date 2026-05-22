9b2e144e3efdc368c00d2c5c67fe277b061f8667
Guilherme G Likoski <159035705+GuilhermeGLikoski@users.noreply.github.com>
2026-05-22 14:49:23 -0300

    Implement word separation from input phrase
    3. Escreva um programa que peça ao usuário uma frase e converta a frase em
    uma lista com cada palavra separada. Imprima a lista na tela.

---
+++ exercicio3.py
@@ -0,0 +1,7 @@
+frase = input("Digite uma frase: ")
+
+lista_palavras = frase.split()
+
+print(f"Lista com as palavras separadas: {lista_palavras}")
+
+#split: pega a frase e separa cada palavra automaticamente em uma lista
